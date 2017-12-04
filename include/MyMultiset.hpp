6b5d7ec1d60090389d1c46d8baca1c5747793048
zimmer_n <nicolas.zimmermann@epitech.eu>
2017-12-04 14:25:46 +0100

    Container MyMultiset created

---
+++ include/MyMultiset.hpp
@@ -0,0 +1,38 @@
+//
+// Created by 53915 on 04/12/2017.
+//
+
+#ifndef GOMOKU_MYMULTISET_HPP
+#define GOMOKU_MYMULTISET_HPP
+
+#include <set>
+#include <utility>
+
+template<class T, class U>
+class MyMultiset {
+public :
+    using compareFunc = bool (*)(std::pair<T, U>, std::pair<T, U>);
+    MyMultiset(const compareFunc &func) : std::multiset<std::pair<T, U>, compareFunc>(func) {}
+
+    U operator[](T key) {
+        for (auto it = m_multiset.begin(); it != m_multiset.end(); it++)
+            if (it->first == key)
+                return it->second;
+    }
+
+    void addInList(T key, U value) {
+        m_multiset.insert(std::make_pair<T, U>(key, value));
+    }
+
+    void erase(T key) {
+        for (auto it = m_multiset.begin(); it != m_multiset.end(); it++) {
+            if (it->first == key)
+                m_multiset.erase(it);
+        }
+    }
+
+private:
+    std::multiset<std::pair<T, U>, bool *(std::pair<T, U>, std::pair<T, U>)> m_multiset;
+};
+
+#endif //GOMOKU_MYMULTISET_HPP
